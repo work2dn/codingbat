7a8255b55896e72116e3626c2cf28855ec953e4d
work2dn <work2darshan@gmail.com>
2020-12-30 13:18:15 +0530

    last problem under heading string -1

--- CODINGBAT/src/com/string_1/WithoutX2.java
+++ CODINGBAT/src/com/string_1/WithoutX2.java
@@ -19,20 +19,62 @@ public class WithoutX2 {
 
 	public static void main(String[] args) {
 
-		String str = "xHi";
+		String str = "Hxi";
 		System.out.println(withoutX2(str));
+		System.out.println(withoutX2Small(str));
+
 		
+	}
+
+	private static String withoutX2Small(String str) {
+
+		String temp = "";
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			if(i == 0 && str.charAt(i) == 'x') { // if(i == 0 && str.charAt(i) != 'x') 
+				
+				temp+="";						// temp+=str.charAt(i);
+				
+			} else if(i == 1 && str.charAt(i) == 'x') { // else if(i == 1 && str.charAt(i) != 'x')
+				
+				temp+="";								// temp+=str.charAt(i);
+					
+			} else {								// else if(i >1)
+				
+				temp+=str.charAt(i); 				// temp+=str.charAt(i)
+				
+			}
+			
+		}
+		
+		return temp;
 	}
 
 	private static String withoutX2(String str) {
 		
-		String firt2Char = str.substring(0, 2);
+		if(str.length() <= 1) {
+			
+			return (str.equals("x")) ? "" : str;
+		}
 		
+		String first2Char = str.substring(0, 2);
 		
-		String left = str.substring(2);
+		String leftChars = str.substring(2);
 		
+		if(first2Char.charAt(0) == 'x') {
+			
+			first2Char = first2Char.substring(1);
+			
+		}
 		
-		return null;
+		if(first2Char.charAt(first2Char.length() - 1) == 'x') {
+			
+			first2Char = first2Char.substring(0, first2Char.length() - 1);
+			
+		}
+		
+		return first2Char + leftChars;
 		
 	}
 
